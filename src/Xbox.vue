2dc49ebb23b5265c6c20971ec3b0ee276d4e755b
Ken Sanders <Kenneth.Sanders@acxiom.com>
2022-02-14 16:19:55 -0600

    Cleaning House

--- src/Xbox.vue
+++
@@ -1,3 +0,0 @@
-<template>
-<pre>Hello, World!</pre>
-</template>
--- update-links.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ln -sf /home/nonbreaking/vue-spa/dist /home/nonbreaking/www/dist
-ln -sf /home/nonbreaking/vue-spa/index.html /home/nonbreaking/www/index.html
-ln -sf /home/nonbreaking/vue-spa/assets /home/nonbreaking/www/assets
